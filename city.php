e1aeeb5438c5134166a187c18c21994ae65137cf
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2024-07-09 22:01:48 +0600

    updated

--- city.php
+++ city.php
@@ -1,31 +1,12 @@
 <?php
 
-use App\WorldCityRepository;
-
+use App\CityApp;
 
 require __DIR__ . '/vendor/autoload.php';
 
 
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=cities;charset=utf8mb4', 'root', '', [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
-} catch (PDOException $e) {
-    // var_dump($e->getMessage());
-    echo 'A problem occured with the database connection...';
-    die();
-}
+$cityApp = new CityApp();
+$cityApp->run();
 
-$id = (int) ($_GET['id'] ?? 0);
 
-$worldCityRepository = new WorldCityRepository($pdo);
-$city = $worldCityRepository->fetchById($id);
 
-if (empty($city)) {
-    header('Location: index.php');
-    die();
-}
-
-render('city.view', [
-    'city' => $city
-]);
